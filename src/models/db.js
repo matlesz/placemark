27ee25b1cfc688fcedd7b73d2bc676bbbf5368b7
matlesz <matlesz@gmail.com>
2022-04-24 19:43:24 +0100

    db & joi-schema updated

--- src/models/db.js
+++ src/models/db.js
@@ -1,36 +1,36 @@
 import { userMemStore } from "./mem/user-mem-store.js";
-import { playlistMemStore } from "./mem/location-mem-store.js";
-import { trackMemStore } from "./mem/geocache-mem-store.js";
+import { geocacheMemStore } from "./mem/geocache-mem-store.js";
+import { locationMemStore } from "./mem/location-mem-store.js";
 import { userJsonStore } from "./json/user-json-store.js";
-import { playlistJsonStore } from "./json/location-json-store.js";
-import { trackJsonStore } from "./json/geocache-json-store.js";
+import { geocacheJsonStore } from "./json/geocache-json-store.js";
+import { locationJsonStore } from "./json/location-json-store.js";
+import {connectMongo} from "./mongo/connect.js"
 import { userMongoStore } from "./mongo/user-mongo-store.js";
+import { geocacheMongoStore } from "./mongo/geocache-mongo-store.js";
 import { locationMongoStore } from "./mongo/location-mongo-store.js";
-import { trackMongoStore } from "./mongo/geocache-mongo-store.js";
-import { connectMongo } from "./mongo/connect.js";
 
 export const db = {
   userStore: null,
-  playlistStore: null,
-  trackStore: null,
+  geocacheStore: null,
+  locationStore:null,
 
   init(storeType) {
-    switch (storeType) {
-      case "json" :
+    switch(storeType) {
+      case "json":
         this.userStore = userJsonStore;
-        this.playlistStore = playlistJsonStore;
-        this.trackStore = trackJsonStore;
+        this.geocacheStore = geocacheJsonStore;
+        this.locationStore=locationJsonStore;
         break;
-      case "mongo" :
+      case "mongo":
         this.userStore = userMongoStore;
-        this.playlistStore = locationMongoStore;
-        this.trackStore = trackMongoStore;
+        this.geocacheStore= geocacheMongoStore;
+        this.locationStore=locationMongoStore;
         connectMongo();
         break;
-      default :
+      default:
         this.userStore = userMemStore;
-        this.playlistStore = playlistMemStore;
-        this.trackStore = trackMemStore;
+        this.geocacheStore = geocacheMemStore;
+        this.locationStore=locationMemStore;
     }
-  }
+  },
 };
--- src/models/joi-schemas.js
+++ src/models/joi-schemas.js
@@ -1,60 +1,61 @@
 import Joi from "joi";
 
 export const IdSpec = Joi.alternatives().try(Joi.string(), Joi.object()).description("a valid ID");
-
 export const UserCredentialsSpec = Joi.object()
-  .keys({
-    email: Joi.string().email().example("homer@simpson.com").required(),
-    password: Joi.string().example("secret").required(),
-  })
-  .label("UserCredentials");
+    .keys({
+        email: Joi.string().email().example("homer@simpson.com").required(),
+        password: Joi.string().example("secret").required(),
+    })
+    .label("UserCredentials");
 
 export const UserSpec = UserCredentialsSpec.keys({
-  firstName: Joi.string().example("Homer").required(),
-  lastName: Joi.string().example("Simpson").required(),
+    firstName: Joi.string().example("Homer").required(),
+    lastName: Joi.string().example("Simpson").required(),
+    role:Joi.string().example("admin").optional(),
 }).label("UserDetails");
 
 export const UserSpecPlus = UserSpec.keys({
-  _id: IdSpec,
-  __v: Joi.number(),
+    _id: IdSpec,
+    __v: Joi.number(),
 }).label("UserDetailsPlus");
 
-export const UserArray = Joi.array().items(UserSpecPlus).label("UserArray");
+export const UserArray = Joi.array().items(UserSpec).label("UserArray");
 
-export const TrackSpec = Joi.object()
-  .keys({
-    title: Joi.string().required().example("Piano Sonata No. 7"),
-    artist: Joi.string().required().example("Beethoven"),
-    duration: Joi.number().allow("").optional().example(12),
-    playlistid: IdSpec,
-  })
-  .label("Track");
+export const LocationSpec = Joi.object()
+    .keys({
+        name: Joi.string().required().example("Loong Avenue"),
+        latitude: Joi.number().example(32).required(),
+        longitude: Joi.number().example(18).required(),
+        category:Joi.string().example("Traditional").required(),
+        size: Joi.string().example("Micro").required(),
 
-export const TrackSpecPlus = TrackSpec.keys({
-  _id: IdSpec,
-  __v: Joi.number(),
-}).label("TrackPlus");
+    })
+    .label("Location");
 
-export const TrackArraySpec = Joi.array().items(TrackSpecPlus).label("TrackArray");
+export const LocationSpecPlus = LocationSpec.keys({
+    _id: IdSpec,
+    __v: Joi.number(),
+}).label("LocationPlus");
+export const LocationArraySpec = Joi.array().items(LocationSpecPlus).label("LocationArray");
 
-export const PlaylistSpec = Joi.object()
-  .keys({
-    title: Joi.string().required().example("Beethoven Sonatas"),
-    userid: IdSpec,
-    tracks: TrackArraySpec,
-  })
-  .label("Playlist");
+export const GeocacheSpec = Joi.object()
+    .keys({
+        name: Joi.string().required().example("Waterford"),
+        userid: IdSpec,
+        locations: LocationArraySpec,
+    })
+    .label("Geocache");
 
-export const PlaylistSpecPlus = PlaylistSpec.keys({
-  _id: IdSpec,
-  __v: Joi.number(),
-}).label("PlaylistPlus");
+export const GeocacheSpecPlus = GeocacheSpec.keys({
+    _id: IdSpec,
+    __v: Joi.number(),
+}).label("GeocachePlus");
 
-export const PlaylistArraySpec = Joi.array().items(PlaylistSpecPlus).label("PlaylistArray");
+export const GeocacheArraySpec = Joi.array().items(GeocacheSpecPlus).label("GeocacheArray");
 
 export const JwtAuth = Joi.object()
-  .keys({
-    success: Joi.boolean().example("true").required(),
-    token: Joi.string().example("eyJhbGciOiJND.g5YmJisIjoiaGYwNTNjAOhE.gCWGmY5-YigQw0DCBo").required(),
-  })
-  .label("JwtAuth");
+    .keys({
+        success: Joi.boolean().example("true").required(),
+        token: Joi.string().example("eyJhbGciOiJND.g5YmJisIjoiaGYwNTNjAOhE.gCWGmY5-YigQw0DCBo").required(),
+    })
+    .label("JwtAuth");
